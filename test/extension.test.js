a0dd8171820093a80fc49c976ea3c82937951afd
u2re-dev <c24b@u2re.ru>
2025-07-14 00:22:55 +0700

    test code

--- test/extension.test.js
+++ test/extension.test.js
@@ -2,9 +2,8 @@ import assert from 'assert';
 import vscodeAPI from '../src/imports/api.ts';
 
 //
-suite('Extension Test Suite', () => {
-	vscodeAPI?.window?.showInformationMessage?.('Start all tests.');
-
+suite('Extension Test Suite', async () => {
+	await (await vscodeAPI)?.window?.showInformationMessage?.('Start all tests.');
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
